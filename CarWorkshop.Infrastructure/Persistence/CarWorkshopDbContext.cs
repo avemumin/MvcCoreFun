9f9274dbe9a7a67638d07de515c83c393d6d634a
Jacek Szczepański <jacogitproject@gmail.com>
2023-09-14 19:53:32 +0200

    Add project files.

---
+++ CarWorkshop.Infrastructure/Persistence/CarWorkshopDbContext.cs
@@ -0,0 +1,23 @@
+﻿using CarWorkshop.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarWorkshop.Infrastructure.Persistence;
+
+public class CarWorkshopDbContext : DbContext
+{
+    
+    public DbSet<Domain.Entities.CarWorkshop> CarWorkshops { get; set; }
+
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var cn = "Server=minisforum\\MSSQLSERVER2022; Database = CarWorkshop; User Id = sa; Password = 5432!qaz; Trusted_Connection = True; Encrypt = False; TrustServerCertificate = True;";
+        optionsBuilder.UseSqlServer(cn);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Domain.Entities.CarWorkshop>()
+            .OwnsOne(c => c.ContactDetails);
+    }
+}
---
+++ CarWorkshop.MVC/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using CarWorkshop.MVC.Models;
+
+namespace CarWorkshop.MVC.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        List<Person> persons = new List<Person>
+        {
+            new (){FirstName = "Staś", LastName="Szczepański"},
+            new (){FirstName = "Malwinka", LastName ="Szczepańska" }
+        };
+
+
+        return View("Privacy", persons);
+    }
+
+    public IActionResult About()
+    {
+        var tagi = new List<Abouts>()
+        {
+            new Abouts
+            {
+                Title = "Jakis",
+                Description = "stek bzdur",
+                Tags = new[] { "firstarg", "secondarg", "thirdarg" }
+            },
+            new Abouts
+            {
+                Title = "Drugi",
+                Description = "same madrosci",
+                Tags = new[] { "1", "2", "3" }
+            }
+        };
+
+
+        return View("About", tagi);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
