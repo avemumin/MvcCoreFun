4b3c63627ff9c1dfc94756abb30b6a513d79fa03
Jacek Szczepański <jacogitproject@gmail.com>
2023-09-30 17:37:16 +0200

    Asp net validation with asp-for

--- CarWorkshop.Application/CarWorkshop/CarWorkshopDto.cs
+++ CarWorkshop.Application/CarWorkshop/CarWorkshopDto.cs
@@ -1,12 +1,17 @@
-﻿namespace CarWorkshop.Application.CarWorkshop
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarWorkshop.Application.CarWorkshop
 {
     public class CarWorkshopDto
     {
+        [Required]
+        [StringLength(30, ErrorMessage = "Name must be min 2 character length", MinimumLength = 2)]
         public string Name { get; set; } = default!;
+        [Required]
         public string? Description { get; set; }
         public string? About { get; set; }
 
-
+        [StringLength(12, MinimumLength = 8)]
         public string? PhoneNumber { get; set; }
         public string? Street { get; set; }
         public string? City { get; set; }
--- CarWorkshop.MVC/Controllers/CarWorkshopController.cs
+++ CarWorkshop.MVC/Controllers/CarWorkshopController.cs
@@ -21,6 +21,10 @@ namespace CarWorkshop.MVC.Controllers
         [HttpPost]
         public async Task<IActionResult> Create(CarWorkshopDto carWorkshop)
         {
+            if(!ModelState.IsValid)
+            {
+                return View(carWorkshop);
+            }
             await _carWorkshopService.Create(carWorkshop);
             return RedirectToAction(nameof(Create));
         }
